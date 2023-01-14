d8f285ae2c331d418dbb6ef636ef98e484ed2e09
RGROHIT9699 <119799265+RGROHIT9699@users.noreply.github.com>
2023-01-14 23:27:18 +0530

    Add files via upload

---
+++ switch_vowel.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	char ait;
+	scanf("%c",&ait);
+	switch (ait)
+	{
+		case 'a': printf("v");
+		break;
+		case 'e': printf("v");
+		break;
+		case 'i': printf("v");
+		break;
+		case 'o': printf("v");
+		break;
+		case 'u': printf("v");
+		break;
+		case 'A': printf("v");
+		break;
+		case 'E': printf("v");
+		break;
+		case 'I': printf("v");
+		break;
+		case 'O': printf("v");
+		break;
+		case 'U': printf("v");
+		break;
+		default: printf("c");
+	}
+	return 0;
+}
